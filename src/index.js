5b073e74259a3450fec9afb8aafa0f32d6cb0478
Alexis Varela <alexisbec13@hotmail.com>
2021-06-08 15:36:44 -0500

    add bootstrap and bare bones html template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('HELLO WORLD !!');
+const projects = [];
+
+class Projects {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  };
+
+  addProject() {
+    
+  }
+}
